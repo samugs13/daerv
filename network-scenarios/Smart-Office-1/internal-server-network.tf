a655de800f4900455b76a202e8a8d5e5aa4500d3
samugs13 <garciasanchezsamu@gmail.com>
2022-05-27 18:12:24 +0200

    renamed file

--- network-scenarios/Smart-Office-1/internal-server-network.tf
+++ network-scenarios/Smart-Office-1/internal-server-network.tf
@@ -1,15 +1,15 @@
-resource "google_compute_network" "server_network" {
+resource "google_compute_network" "internal_server_network" {
   project                 = var.project_id
   name                    = "server-network"
   mtu                     = 1460
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "server_lan" {
-  name          = "server-lan"
-  ip_cidr_range = var.private_subnet_cidr_blocks[4]
+resource "google_compute_subnetwork" "printer_server_lan" {
+  name          = "printer-server-lan"
+  ip_cidr_range = var.private_subnet_cidr_blocks[2]
   region        = var.region
-  network       = google_compute_network.server_network.self_link
+  network       = google_compute_network.internal_server_network.self_link
 }
 
 resource "google_compute_instance" "cloud_printer_server" {
@@ -17,7 +17,7 @@ resource "google_compute_instance" "cloud_printer_server" {
   machine_type = var.machine_type
   project      = var.project_id
   zone         = var.zone
-  tags         = ["server"]
+  tags         = ["ssh", "server"]
 
   boot_disk {
     initialize_params {
@@ -26,8 +26,7 @@ resource "google_compute_instance" "cloud_printer_server" {
   }
 
   network_interface {
-    subnetwork = google_compute_subnetwork.server_lan.self_link
-    access_config {}
+    subnetwork = google_compute_subnetwork.printer_server_lan.self_link
   }
 
   metadata_startup_script = templatefile(var.manual_provisioning_path, { args = "-p 80:80", image = "nginx", tag = "latest" })
